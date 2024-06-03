32e1eeffa90fcc3b8b72a75b76be71a9d63dc571
wirefailed <junsoo.kim.college@gmail.com>
2024-06-02 21:51:34 -0700

    Little updates before calling it a day

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
+import express from "express";
+
+const app = express();
+const port = 3000;
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+app.listen(port, () => {
+    console.log("Server running on port " + port +".")
+});
